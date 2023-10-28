2b48fa0f3f6775f4e6fc6039a9e28a7798e6dd13
Ayda <136740721+aydaghasemi66@users.noreply.github.com>
2023-10-28 05:26:36 -0700

    first commit:)

---
+++ root/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+
+app_name = 'root'
+
+urlpatterns = [
+    path("",HomeView.as_view(),name="home"),
+    #path("about",about,name="about"),
+    #path("trainer",trainer,name="trainer")
+]
