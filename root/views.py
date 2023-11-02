be40bf7ff150297443ae8e1ff6bbac9c89cd4e85
Ayda <136740721+aydaghasemi66@users.noreply.github.com>
2023-11-02 00:34:32 -0700

    Update views.py

--- root/views.py
+++ root/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.db import models
 
-# Create your views here.
-class HomeView(TemplateView):
-    template_name = 'root/index.html'
+# Create your models here.
 
-class UserView(TemplateView):
-    template_name = 'profile/personal-info.html'
+class Services (models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_data = models.DateTimeField(auto_now_add=True)
+    updated_data = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created_data']
+
+
+class NewsLetter (models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+    
+
+class ContactUs (models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+
+    def __str__(self):
+        return self.name
